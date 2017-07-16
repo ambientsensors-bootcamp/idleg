d882457bedd56a76ce6163c314491c5bbec03381
Megan <Megan@Timothys-MacBook-Pro-2.local>
2017-07-16 15:49:11 -0600

    import material

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MaterialModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -9,15 +10,9 @@ import { MenuComponent } from './components/menu/menu.component';
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    HeaderComponent
-=======
-<<<<<<< HEAD
-    CommentBoxComponent
-=======
+    HeaderComponent,
+    CommentBoxComponent,
     MenuComponent
->>>>>>> 2b48498ee6ff1e6ec02c168b79c94d0b904d5bba
->>>>>>> 44f68e8bc0431c5b76ee9689497b3f7cebe2beed
   ],
   imports: [
     BrowserModule
