3537b98f14679d4593d11f9e3f7a1b4e044a9fcc
Nathaniel Hoffman <nhoffman@healthwise.org>
2017-07-16 15:21:30 -0600

    commentBox added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CommentBoxComponent } from './components/comment-box/comment-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CommentBoxComponent
   ],
   imports: [
     BrowserModule
