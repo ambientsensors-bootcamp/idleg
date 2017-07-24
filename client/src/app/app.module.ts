512e3cf2a5b9c3ad91f6266b471cc8229ddd35c5
suadad <suadadjasim@jasimsuadad@u.boisestate.edu>
2017-07-23 19:15:01 -0600

    moving the use of bill-list to bills component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {MdButtonModule, MdCheckboxModule} from '@angular/material';
 import { MaterialModule } from '@angular/material';
 import { HeaderComponent } from './components/header/header.component';
 import { CommentBoxComponent } from './components/comment-box/comment-box.component';
@@ -12,6 +13,7 @@ import { AboutComponent } from './components/about/about.component';
 import { BillsComponent } from './components/bills/bills.component';
 import {RouterModule,Routes} from '@angular/router';
 import { BillDataService } from './bill-data.service';
+
 const appRoutes:Routes=[
   {path:'',component:AppComponent},
   {path:'about',component:AboutComponent},
@@ -35,7 +37,9 @@ const appRoutes:Routes=[
 
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    MaterialModule
+    
   ],
   providers: [BillDataService],
   bootstrap: [AppComponent]
