2d995d1a23a68a6b0e3ebfff453a7ccb190b46c0
suadad <suadadjasim@jasimsuadad@u.boisestate.edu>
2017-07-25 21:43:44 -0600

    small changes on bill-list

--- client/src/app/components/bill-list/bill-list.component.ts
+++ client/src/app/components/bill-list/bill-list.component.ts
@@ -9,15 +9,25 @@ export class BillListComponent implements OnInit {
   
   bills:Bill[];
 
+  /* submitted = false;
+  onSu bmit() { this.submitted = true; }*/
+
   constructor() {
     this.bills = [];
   }
 
   ngOnInit() {
     for  (let i=0;i<=3;i++){
-      this.bills.push({Id:'hh',body:'my bill number',userComment:'kk',userLike:i});
+      this.bills.push({Id:i.toString(),
+      body:'The status of each bill, resolution, proclamation, and memorials listed on this page are updated when the offices of the Secretary of the Senate and the Chief Clerk of the House publish the un-official daily journals and should not be deemed official. The official bill actions are located in the final journal, which are maintained by the offices of the Secretary of the Senate and the Chief Clerk of the House.'+i.toString(),
+      userComment:'kk'+i.toString(),userLike:i});
     }
     
+  }
+
+  
+  addComment(comment){
+   
   }
 
 }
