d4c5325af282ca08ba9b7c1b3aaa3e7b782a52ea
Nathaniel Hoffman <nhoffman@gmail.com>
2017-07-30 14:25:48 -0600

    API calls display on cards

--- client/src/app/components/bill-list/bill-list.component.ts
+++ client/src/app/components/bill-list/bill-list.component.ts
@@ -18,29 +18,23 @@ export class BillListComponent implements OnInit {
   }
 
   ngOnInit() {
-    // for  (let i=0;i<=3;i++){
-    //this.bills.push({Id:i.toString(),
-    // body:'The status of each bill, resolution, proclamation, and memorials listed on this page are updated when the offices of the Secretary of the Senate and the Chief Clerk of the House publish the un-official daily journals and should not be deemed official. The official bill actions are located in the final journal, which are maintained by the offices of the Secretary of the Senate and the Chief Clerk of the House.'+i.toString(),
-    //userComment:'kk'+i.toString(),userLike:i});
-
     this.ws.getResource("bills").subscribe(data => {
       this.bills = data;
-      console.log("data", this.bills);
+      console.log("data", this.bills); 
 });
-    //}
-    
-  }
+    /* for  (let i=0;i<=3;i++){
+    this.bills.push({Id:i.toString(),
+    body:'The status of each bill, resolution, proclamation, and memorials listed on this page are updated when the offices of the Secretary of the Senate and the Chief Clerk of the House publish the un-official daily journals and should not be deemed official. The official bill actions are located in the final journal, which are maintained by the offices of the Secretary of the Senate and the Chief Clerk of the House.'+i.toString(),
+    userComment:'kk'+i.toString(),userLike:i});  */
 
-  
-  addComment(comment){
-   
+  }    
   }
-
-}
 
 interface Bill{
-  Id:string,
-  body:string,
-  userComment:string,
-  userLike:number
+  bill_id:string,
+  title:string,
+  id:string,
+  session: string,
+  created_at:string,
+  updated_at:string
 }
