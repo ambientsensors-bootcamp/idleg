1c05840d91f28982c6694d81b8054d556ec4baf0
nmousa <nmousa@onyx.boisestate.edu>
2017-08-27 15:25:02 -0600

    Add config files because why not? Deal with it

---
+++ src/assets/config.js
@@ -0,0 +1,6 @@
+var config = {};
+
+config.dbUser = "idleg";
+config.dbPass = "dMjy$8AuG3V7n";
+
+module.exports = config;
---
+++ src/assets/config.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+// test comment to see if pushed
+
+@Injectable()
+export class ImportantConfig {
+    public API_KEY = "b86ea60a-aa79-4ccf-8a5f-82fe1b198c0c";
+}
